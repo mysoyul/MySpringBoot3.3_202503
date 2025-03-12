ab7fef85342c58a3696e86bdb202d9eff416729b
mysoyul <vega2k_@naver.com>
2025-03-12 14:51:03 +0900

    환경Properties add

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,8 +1,11 @@
 package com.basic.myspringboot.runner;
 
+import com.basic.myspringboot.property.MybootProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,9 +16,22 @@ public class MyRunner implements ApplicationRunner {
     @Value("${myboot.age}")
     int age;
 
+    @Autowired
+    Environment env;
+
+    @Autowired
+    MybootProperties properties;
+
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("myboot.name " + name);
-        System.out.println("myboot.age " + age);
+        System.out.println("@Value myboot.name " + name);
+        System.out.println("@Value myboot.age " + age);
+        System.out.println("Environment myboot.fullName " +
+                env.getProperty("myboot.fullName"));
+        System.out.println("Environment server.port " +
+                env.getProperty("local.server.port"));
+        System.out.println("MybootProperties getFullName " +
+                properties.getFullName());
+
         System.out.println("VM Arguments foo : " + args.containsOption("foo"));
         System.out.println("Program Arguments bar : " + args.containsOption("bar"));
 
