a80f75ca090536075809e5d603b1cfaf084c10e2
mysoyul <vega2k_@naver.com>
2025-03-12 17:39:06 +0900

    Entity Repository add

--- src/main/java/com/basic/myspringboot/entity/Customer.java
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -5,14 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "customers")
 @Getter
 @Setter
 public class Customer {
-    @Id
+    @Id  //Primary Key
+    //pk의 sequential 값을 자동으로 증가하는 전략
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+
+    @Column(unique = true, nullable = false, name = "cust_id")
     private String customerId;
-    @Column
+
+    @Column(nullable = false, name = "cust_name")
     private String customerName;
 }
