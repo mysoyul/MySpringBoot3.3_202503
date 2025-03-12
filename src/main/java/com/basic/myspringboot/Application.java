587609a25979c1deeacad3e2bcd295dcda4f9fb1
mysoyul <vega2k_@naver.com>
2025-03-12 12:21:35 +0900

    springboot3.3 first commit

---
+++ src/main/java/com/basic/myspringboot/Application.java
@@ -0,0 +1,20 @@
+package com.basic.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public String hello() {
+		return "Hello Springboot";
+	}
+
+}
