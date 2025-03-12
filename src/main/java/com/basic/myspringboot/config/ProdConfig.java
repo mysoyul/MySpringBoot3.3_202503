b83166a0746f3927254de5e016c299ccd0b51f04
mysoyul <vega2k_@naver.com>
2025-03-12 14:53:13 +0900

    Profile add

---
+++ src/main/java/com/basic/myspringboot/config/ProdConfig.java
@@ -0,0 +1,17 @@
+package com.basic.myspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("prod")
+@Configuration
+public class ProdConfig {
+    @Bean
+    public CustomVO customVO() {
+        return CustomVO.builder()
+                .mode("운영 환경")
+                .rate(0.8)
+                .build();
+    }
+}
