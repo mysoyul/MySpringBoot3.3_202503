763460dbf0dd8497cb21353daa89ff7549f04fb2
mysoyul <vega2k_@naver.com>
2025-03-12 16:30:17 +0900

    Customer Entity add

---
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -0,0 +1,18 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String customerId;
+    @Column
+    private String customerName;
+}
