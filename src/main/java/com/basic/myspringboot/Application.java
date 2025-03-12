c90e684311a36a11d979cbe4bb349e302c449db1
mysoyul <vega2k_@naver.com>
2025-03-12 12:49:27 +0900

    banner.txt add

--- src/main/java/com/basic/myspringboot/Application.java
+++ src/main/java/com/basic/myspringboot/Application.java
@@ -1,6 +1,7 @@
 package com.basic.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -9,7 +10,16 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		//WebApplication 의 타입 변경
+		/*
+			NONE, - 웹어플리케이션이 아님 (tomcat 동작하지 않음)
+    		SERVLET,(default) SpringMVC
+    		REACTIVE; WebFlux (async, non-blocking)
+		 */
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
 	}
 
 	@Bean
