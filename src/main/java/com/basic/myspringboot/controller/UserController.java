b4b4e820955deef6d52be25c2e627529c271f4c6
mysoyul <vega2k_@naver.com>
2025-03-13 16:02:30 +0900

    UserController index add

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,11 +1,23 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        return new ModelAndView("index","users",userRepository.findAll());
+    }
+
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name", "스프링부트");
