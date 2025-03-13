07d1cb12cec21afaae7e0bd72d20e739d414c2dd
mysoyul <vega2k_@naver.com>
2025-03-13 11:21:03 +0900

    UserRestController add

---
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -0,0 +1,35 @@
+package com.basic.myspringboot.controller;
+
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Slf4j
+@RequiredArgsConstructor
+public class UserRestController {
+    private final UserRepository userRepository;
+
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//        log.info("UserRepository 객체 {}",userRepository.getClass().getName());
+//    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+
+}
